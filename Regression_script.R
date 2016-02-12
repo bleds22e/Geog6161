2dd5cefaf03fbda74dc2dd4516f45ec9774b10dd
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-02-12 16:22:10 -0500

    Trying to find the appropriate transformations

--- Regression_script.R
+++ Regression_script.R
@@ -2,3 +2,110 @@
 # GEO6161 Lab 1 script
 # Initiated: Feb. 12, 2016
 
+# load library
+library(dplyr)
+library(ggplot2)
+
+# read in file
+data <- read.csv(file = "Lab_data.csv", header = TRUE)
+
+# approach
+  # scatterplots of variables against Y
+  # check assumptions
+  # add interactions?
+  # run AIC and stepwise regressions to determine model of best fit
+
+# prep the dataset
+colnames(data) <- c("obs", "mortpay", "income", "sqfoot", "loans", "age")
+
+# scatterplots of variables against Y
+ggplot(data, aes(x = income, y = mortpay)) +
+  geom_point(size = 2) +
+  geom_smooth() +
+  xlab("Household Disposable Income ($1000)") +
+  ylab("Monthly Mortgage Payment ($)") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), panel.grid.major = element_line(colour = "light gray"))
+ggsave(file = "mortpay_vs_income.png", width = 5, height = 5)
+
+ggplot(data, aes(x = sqfoot, y = mortpay)) +
+  geom_point(size = 2) +
+  geom_smooth() +
+  xlab("Square Footage") +
+  ylab("Monthly Mortgage Payment ($)") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), panel.grid.major = element_line(colour = "light gray"))
+ggsave(file = "mortpay_vs_sqfoot.png", width = 5, height = 5)
+
+ggplot(data, aes(x = age, y = mortpay)) +
+  geom_point(size = 2) +
+  geom_smooth() +
+  xlab("Age of Housing Unit (Years)") +
+  ylab("Monthly Mortgage Payment ($)") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), panel.grid.major = element_line(colour = "light gray"))
+ggsave(file = "mortpay_vs_age.png", width = 5, height = 5)
+
+# histograms of independent variables
+ggplot(data, aes(x = income)) +
+  geom_histogram() +
+  xlab("Household Disposable Income ($1000)") +
+  ylab("Frequency") 
+
+ggplot(data, aes(x = sqfoot)) +
+  geom_histogram() +
+  xlab("Square Footage") +
+  ylab("Frequency") 
+
+ggplot(data, aes(x = age)) +
+  geom_histogram() +
+  xlab("Age of Housing Unit (Years)") +
+  ylab("Frequency") 
+
+# log transform variables
+data <- select(data, obs:age) %>% 
+        mutate(log_income = log(income)) %>% 
+        mutate(log_sqfoot = log(sqfoot))
+
+# sqrt transform
+data <- select(data, obs:log_sqfoot) %>% 
+        mutate(sq_income = sqrt(income)) %>% 
+        mutate(sq_sqfoot = sqrt(sqfoot)) %>% 
+        mutate(recip_income = (1/income)) %>% 
+        mutate(recip_sqfoot = (1/sqfoot)) %>% 
+        mutate(cube_root_age = (age^(1/3)))
+head(data)
+data <- select(data, obs:cube_root_age) %>% 
+        mutate(income2 = income^2) %>% 
+        mutate(sqfoot2 = sqfoot^2) %>% 
+        mutate(age3 = age^3) %>% 
+        mutate(ageneg3 = age^-3) %>% 
+        mutate(log10_income = log10(income)) %>% 
+        mutate(log10_sqfoot = log10(sqfoot)) %>% 
+        mutate(log10_mortpay = log10(mortpay)) %>% 
+        mutate(recip_root_income = income^(-.5)) %>% 
+        mutate(recip_root_sqfoot = sqfoot^(-.5)) %>% 
+        mutate(recip_sq_income = income^(-2)) %>% 
+        mutate(recip_sq_sqfoot = sqfoot^(-2)) %>% 
+        mutate(income3 = income^3)
+  
+plot(data = data, mortpay ~ sqfoot2)
+hist(data$log10_mortpay)
+
+ggplot(data, aes(x = age, y = mortpay)) +
+  geom_point(size = 2) +
+  geom_smooth() +
+  xlab("Household Disposable Income ($1000)") +
+  ylab("Monthly Mortgage Payment ($)") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), panel.grid.major = element_line(colour = "light gray"))
+
+# look at predicted vs residuals
+reg <- lm(data$mortpay ~ data$income)
+abline(reg, col="red", lwd=3) 
+predicted<-predict.lm(reg)
+residuals<-data$mortpay-predicted
+plot(predicted,residuals,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+dev.copy(png, "residuals_vs_predicted_income.png")
+dev.off()
+stdRes = rstandard(reg)
+qqnorm(stdRes,ylab="Standardized Residuals", xlab="Theoretical Quantiles")
+qqline(stdRes, col=2,lwd=2)
+hist(stdRes)
