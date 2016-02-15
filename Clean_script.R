2a3d7d1f340f6d3494ef5de95de8a35b303e6d43
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2016-02-15 12:35:22 -0500

    New code for pred vs resid and QQ plots for transformed variables

--- Clean_script.R
+++ Clean_script.R
@@ -63,9 +63,8 @@ data <- select(data, obs:age) %>%
   mutate(age_cube_root = age^(-1/3)) %>% 
   mutate(log10_mortpay = log10(mortpay))
 
-# run regressions and plot pred. vs redis. on them
-
-# income regression
+### run regressions and plot pred. vs redis. & qqplots for NON-transformed variables
+## income regression
 plot(log10_mortpay ~ income_cube, data = data)
 reg_income <- lm(log10_mortpay ~ income_cube, data = data)
 abline(reg_income, col="red", lwd=3)
@@ -78,28 +77,225 @@ abline(a=0,b=0, col="red", lwd=3,lty="dashed")
 ggplot(data, aes(y = residuals_income, x = predicted_income)) +
   geom_point(size = 2) +
   geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
   ylab("Residuals") +
   xlab("Predicted Y") + 
-  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14))
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
 ggsave(file = "pred_res_income.png", width = 5, height = 5)
 
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "income_qq.png", width = 5, height = 5)
+  return(p)
+}
 
-# sqfoot regression
+ggQQ(reg_income)
+
+## sqfoot regression
 plot(log10_mortpay ~ sqfoot, data = data)
 reg_sqfoot <- lm(log10_mortpay ~ sqfoot, data = data)
-abline(reg_sqfoot, col = "red", lwd = 3)
+abline(reg_sqfoot, col="red", lwd=3)
 
 predicted_sqfoot <- predict.lm(reg_sqfoot)
-residuals_sqfoot <- data$log10_mortpay - predicted_sqfoot
-plot(predicted_sqfoot, residuals_sqfoot, cex=2, cex.lab=1.5, cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+residuals_sqfoot <- data$log10_mortpay-predicted_sqfoot
+plot(predicted_sqfoot,residuals_sqfoot,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
 abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+
+ggplot(data, aes(y = residuals_sqfoot, x = predicted_sqfoot)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
+  ylab("Residuals") +
+  xlab("Predicted Y") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+ggsave(file = "pred_res_sqfoot.png", width = 5, height = 5)
+
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "sqfoot_qq.png", width = 5, height = 5)
+  return(p)
+}
+
+ggQQ(reg_sqfoot)
 
 # age regression
 plot(log10_mortpay ~ age_cube_root, data = data)
 reg_age <- lm(log10_mortpay ~ age_cube_root, data = data)
-abline(reg_age, col = "red", lwd = 3)
+abline(reg_age, col="red", lwd=3)
 
 predicted_age <- predict.lm(reg_age)
-residuals_age <- data$log10_mortpay - predicted_age
-plot(predicted_age, residuals_age, cex=2, cex.lab=1.5, cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+residuals_age <- data$log10_mortpay-predicted_age
+plot(predicted_age,residuals_age,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
 abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+
+ggplot(data, aes(y = residuals_age, x = predicted_age)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
+  ylab("Residuals") +
+  xlab("Predicted Y") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+ggsave(file = "pred_res_age.png", width = 5, height = 5)
+
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "age_qq.png", width = 5, height = 5)
+  return(p)
+}
+
+ggQQ(reg_age)
+
+### run regressions and plot pred. vs redis. & qqplots for transformed variables
+
+## income regression
+plot(log10_mortpay ~ income_cube, data = data)
+reg_income_trans <- lm(log10_mortpay ~ income_cube, data = data)
+abline(reg_income, col="red", lwd=3)
+
+predicted_income_trans <- predict.lm(reg_income_trans)
+residuals_income_trans <- data$log10_mortpay-predicted_income_trans
+plot(predicted_income_trans,residuals_income_trans,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+
+ggplot(data, aes(y = residuals_income_trans, x = predicted_income_trans)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
+  ylab("Residuals") +
+  xlab("Predicted Y") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+ggsave(file = "pred_res_income_transformed.png", width = 5, height = 5)
+
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "income_qq_transformed.png", width = 5, height = 5)
+  return(p)
+}
+
+ggQQ(reg_income_trans)
+
+## sqfoot regression
+plot(log10_mortpay ~ sqfoot, data = data)
+reg_sqfoot_trans <- lm(log10_mortpay ~ sqfoot, data = data)
+abline(reg_sqfoot_trans, col="red", lwd=3)
+
+predicted_sqfoot_trans <- predict.lm(reg_sqfoot_trans)
+residuals_sqfoot_trans <- data$log10_mortpay-predicted_sqfoot_trans
+plot(predicted_sqfoot_trans,residuals_sqfoot_trans,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+
+ggplot(data, aes(y = residuals_sqfoot_trans, x = predicted_sqfoot_trans)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
+  ylab("Residuals") +
+  xlab("Predicted Y") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+ggsave(file = "pred_res_sqfoot_transformed.png", width = 5, height = 5)
+
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "sqfoot_qq_transformed.png", width = 5, height = 5)
+  return(p)
+}
+
+ggQQ(reg_sqfoot_trans)
+
+# age regression
+plot(log10_mortpay ~ age_cube_root, data = data)
+reg_age_trans <- lm(log10_mortpay ~ age_cube_root, data = data)
+abline(reg_age_trans, col="red", lwd=3)
+
+predicted_age_trans <- predict.lm(reg_age_trans)
+residuals_age_trans <- data$log10_mortpay - predicted_age_trans
+plot(predicted_age_trans,residuals_age_trans,cex=2,cex.lab=1.5,cex.axis=1.15, ylab=" Residuals", xlab= "Predicted Y")
+abline(a=0,b=0, col="red", lwd=3,lty="dashed")
+
+ggplot(data, aes(y = residuals_age_trans, x = predicted_age_trans)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 0, colour = 'red', linetype = "dashed", size = 1) +
+  ggtitle("Residuals vs. Predicted Values") +
+  ylab("Residuals") +
+  xlab("Predicted Y") + 
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+ggsave(file = "pred_res_age_transformed.png", width = 5, height = 5)
+
+# qqplot in ggplot2
+ggQQ <- function(LM) # argument: a linear model
+{
+  y <- quantile(LM$resid[!is.na(LM$resid)], c(0.25, 0.75))
+  x <- qnorm(c(0.25, 0.75))
+  slope <- diff(y)/diff(x)
+  int <- y[1L] - slope * x[1L]
+  p <- ggplot(LM, aes(sample = .resid)) +
+    stat_qq(alpha = 0.5) +
+    geom_abline(slope = slope, intercept = int, color="red", size = 1) +
+    xlab("Theoretical Quantiles") +
+    ylab("Standardized Residuals") +
+    ggtitle("Normal Q-Q Plot") +
+    theme(panel.background = element_blank(), axis.line = element_line(colour = "black"), axis.title = element_text(size=14), plot.title = element_text(size = 18, face = "bold"))
+  ggsave(filename = "age_qq_transformed.png", width = 5, height = 5)
+  return(p)
+}
+
+ggQQ(reg_age_trans)
